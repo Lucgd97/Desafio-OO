65f53da68d9b986d4ef02cacd46bcd630e86e316
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-05-04 07:21:52 -0300

    Criando Classe Pessoa

---
+++ Desafio-OO/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio_OO
+{
+    public class Pessoa
+    {
+        public Pessoa() { } // construtor pois tem mesmo nome da classe
+
+        //atributos
+        public int Id { get; set; } 
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+
+    }
+}
---
+++ Desafio-OO/Program.cs
@@ -0,0 +1,34 @@
+﻿using Alunos;
+using System;
+using System.Collections.Generic;
+
+namespace Desafio_OO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa danilo = new Pessoa();
+            danilo.Id = 1;
+            danilo.Nome = "Danilo Aparecido";
+            danilo.Endereco = "Rua 1234";
+
+            Pessoa lucas = new Pessoa();
+            lucas.Id = 2;
+            lucas.Nome = "lucas ducci";
+            lucas.Endereco = "Rua 1234";
+
+            Pessoa jean = new Pessoa();
+            jean.Id = 3;
+            jean.Nome = "jean araujo";
+            jean.Endereco = "Rua 1234";
+
+            List<Pessoa> pessoas = new List<Pessoa>();
+            pessoas.Add(danilo);
+            pessoas.Add(lucas);
+            pessoas.Add(jean);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
