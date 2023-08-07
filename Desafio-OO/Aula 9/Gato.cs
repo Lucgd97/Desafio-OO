47e76a3909d2cc3610097a1641b5ddbd1579a110
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-08-07 07:15:50 -0300

    Excercitando abastratação

---
+++ Desafio-OO/Aula 9/Gato.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio_OO.Aula_9
+{
+    class Gato : IAnimal
+    {
+        public string Nome { get; set; }
+        public void Andar()
+        {
+            if (string.IsNullOrEmpty(this.Nome)) // mesma coisa que o if abaixo 
+            {
+
+            }
+
+            //if (this.Nome == "" || this.Nome != null)
+            //{
+
+            //} 
+            //throw new NotImplementedException();  exception
+        }
+
+        public void Correr()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Desafio-OO/Aula 9/IAnimal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio_OO.Aula_9
+{
+    public interface  IAnimal
+    {
+        void Andar();
+        void Correr();
+    }
+}
